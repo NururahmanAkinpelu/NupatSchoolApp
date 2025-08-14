8cfaebb04c0b826c3993952c38f98c2635854a5a
unstoppable22 <tboybade@gmail.com>
2025-08-13 23:37:11 -1200

    feat: teacher functions

---
+++ Interface/ITeacherService.cs
@@ -0,0 +1,12 @@
+﻿using NupatSchoolApp.Models.Entities;
+
+namespace NupatSchoolApp.Interface
+{
+    public interface ITeacherService
+    {
+        void AddTeacher(string firstName, string lastName, string level, string course, string email);
+        List<Teacher> GetTeachers();
+        List<Teacher> GetTeachers(string parameter);
+        Teacher GetTeacher(string teacherId);
+    }
+}
